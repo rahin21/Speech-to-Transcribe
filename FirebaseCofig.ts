6aebb3c531a599dea580cc0e09710961a82955a6
rahin21 <mdrahinzaman21@gmail.com>
2024-02-02 20:03:47 +0600

    firebase fileupload

---
+++ FirebaseCofig.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbMu7CFSb1U_xiSc0bScSqvG40kChF5gM",
+  authDomain: "transcription-d9c70.firebaseapp.com",
+  projectId: "transcription-d9c70",
+  storageBucket: "transcription-d9c70.appspot.com",
+  messagingSenderId: "650121212359",
+  appId: "1:650121212359:web:7741b9d6da4a2830f629e2",
+  measurementId: "G-WL4V7V6YXY"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,51 +5,44 @@ import {
   LiveTranscriptionEvents,
   createClient,
 } from "@deepgram/sdk";
-
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 import { Text, View } from "@/components/Themed";
 import { Audio } from "expo-av";
 import { useEffect, useState } from "react";
+import { app } from "@/FirebaseCofig";
+const storage = getStorage(app,"gs://transcription-d9c70.appspot.com");
+import * as FileSystem from 'expo-file-system'
 
 export default function TabOneScreen() {
-  
+  const fetch = require("cross-fetch");
   const eventEmitter = new NativeEventEmitter();
   const [recording, setRecording] = useState<any>();
   const [recordings, setRecordings] = useState<any>([]);
+  const [key, setKey] = useState<any>([]);
   const [permissionResponse, requestPermission] = Audio.usePermissions();
   const [apiKey, setApiKey] = useState<CreateProjectKeyResponse | null>();
-  
-  useEffect(() => {
-    if (!apiKey) {
-      console.log("getting a new api key");
-      fetch("/api", { cache: "no-store" })
-        .then((res) => res.json())
-        .then((object) => {
-          if (!("key" in object)) throw new Error("No api key returned");
-          setApiKey(object);
-        })
-        .catch((e) => {
-          console.error(e);
-        });
-    }
-  }, [apiKey]);
+  const [connection, setConnection] = useState<LiveClient | null>();
+  const [caption, setCaption] = useState<string | null>();
+  const [isListening, setListening] = useState(false);
  
   async function startRecording() {
     try {
-      if (permissionResponse?.status !== 'granted') {
-        console.log('Requesting permission..');
+      if (permissionResponse?.status !== "granted") {
+        console.log("Requesting permission..");
         await requestPermission();
       }
       await Audio.setAudioModeAsync({
         allowsRecordingIOS: true,
         playsInSilentModeIOS: true,
       });
-        const { recording } = await Audio.Recording.createAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY);
-        setRecording(recording);
-        console.log('Recording started');
-      }
-     catch (err) {
-      console.error('Failed to start recording', err);
-     }
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RecordingOptionsPresets.HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log("Recording started");
+    } catch (err) {
+      console.error("Failed to start recording", err);
+    }
   }
 
   async function stopRecording() {
@@ -61,33 +54,49 @@ export default function TabOneScreen() {
     allRecordings.push({
       sound: sound,
       duration: getDurationFormatted(status.durationMillis),
-      file: recording.getURI()
+      file: recording.getURI(),
     });
-
+    
+    const response = await fetch(recording.getURI());
+    const file = await response.blob();
+    const filename = recording.getURI().substring(recording.getURI().lastIndexOf('/')+1);
+    const audioRef = ref(storage, filename);
+    const snapshot = await uploadBytes(audioRef, filename)
+// console.log(recording)
+// console.log(audioRef)
+    uploadBytes(audioRef, file).then((snapshot) => {
+      console.log('Uploaded a blob or file!');
+    });
     setRecordings(allRecordings);
   }
 
-  function getDurationFormatted(milliseconds:number) {
+  function getDurationFormatted(milliseconds: number) {
     const minutes = milliseconds / 1000 / 60;
     const seconds = Math.round((minutes - Math.floor(minutes)) * 60);
-    return seconds < 10 ? `${Math.floor(minutes)}:0${seconds}` : `${Math.floor(minutes)}:${seconds}`
+    return seconds < 10
+      ? `${Math.floor(minutes)}:0${seconds}`
+      : `${Math.floor(minutes)}:${seconds}`;
   }
 
   function getRecordingLines() {
-    return recordings.map((recordingLine:any, index:number) => {
+    return recordings.map((recordingLine: any, index: number) => {
       return (
         <View key={index} style={styles.row}>
           <Text style={styles.fill}>
             Recording #{index + 1} | {recordingLine.duration}
           </Text>
-          <Button onPress={async() => await recordingLine.sound.replayAsync()} title="Play"></Button>
+          <Button
+            onPress={async () => await recordingLine.sound.replayAsync()}
+            title="Play"
+          ></Button>
         </View>
       );
     });
   }
+  
 
   function clearRecordings() {
-    setRecordings([])
+    setRecordings([]);
   }
 
   return (
@@ -98,13 +107,16 @@ export default function TabOneScreen() {
         lightColor="#aae"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <Button title={recording ? 'Stop Recording' : 'Start Recording'} onPress={recording ? stopRecording : startRecording} />
+      <Button
+        title={recording ? "Stop Recording" : "Start Recording"}
+        onPress={recording ? stopRecording : startRecording}
+      />
       {getRecordingLines()}
-      {recordings.length > 0 ? 
-      <Button title={'Clear Recordings'} onPress={clearRecordings} />
-      :
-      <View/>
-      }
+      {recordings.length > 0 ? (
+        <Button title={"Clear Recordings"} onPress={clearRecordings} />
+      ) : (
+        <View />
+      )}
     </View>
   );
 }
@@ -125,14 +137,14 @@ const styles = StyleSheet.create({
     width: "80%",
   },
   row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     marginLeft: 10,
-    marginRight: 40
+    marginRight: 40,
   },
   fill: {
     flex: 1,
-    margin: 15
-  }
+    margin: 15,
+  },
 });
